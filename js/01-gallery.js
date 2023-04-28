a9c73463d0dd4e1ca2f9d5b4599060c5061e0dc0
YanLozovskyi <yanlozovskiy@gmail.com>
2023-04-28 22:07:11 +0300

    add markUp

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const imageContainer = document.querySelector(".gallery");
+const cardsMarkup = createImageCardsMarkup(galleryItems);
+
+imageContainer.insertAdjacentHTML("beforeend", cardsMarkup);
+
+imageContainer.addEventListener("click", onImageContainerClick);
+
+function createImageCardsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      // const { preview, original, description } = item;
+      return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+    })
+    .join("");
+}
+
+function onImageContainerClick(evt) {
+  evt.preventDefault();
+  const isGalleryImageEl = evt.target.classList.contains("gallery__image");
+  if (!isGalleryImageEl) {
+    return;
+  }
+
+  const parent = evt.target.closest(".gallery__link");
+  
+}
