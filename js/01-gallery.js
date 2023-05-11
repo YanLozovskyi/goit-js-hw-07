1b11463f9187a3663886a6924b67af5c0a5a4106
YanLozovskyi <yanlozovskiy@gmail.com>
2023-05-11 10:19:38 +0300

    add comments

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,19 +1,22 @@
+// импортируем массив с данными для галереи из внешнего модуля
 import { galleryItems } from "./gallery-items.js";
-// Change code below this line
 
+// выводим содержимое массива в консоль для проверки
 console.log(galleryItems);
 
+// получаем элемент контейнера галереи и генерируем разметку карточек с изображениями
 const imageContainer = document.querySelector(".gallery");
 const cardsMarkup = createImageCardsMarkup(galleryItems);
 
+// добавляем разметку карточек в контейнер галереи
 imageContainer.insertAdjacentHTML("beforeend", cardsMarkup);
-
+// добавляем обработчик события клика на контейнер галереи
 imageContainer.addEventListener("click", onImageContainerClick);
 
+// функция для генерации разметки карточек изображений
 function createImageCardsMarkup(Items) {
-  return Items
-    .map(({ preview, original, description }) => {
-      return `<li class="gallery__item">
+  return Items.map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
   <a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
@@ -23,33 +26,39 @@ function createImageCardsMarkup(Items) {
     />
   </a>
 </li>`;
-    })
-    .join("");
+  }).join("");
 }
 
+// функция обработки клика на контейнер галереи
 function onImageContainerClick(evt) {
   evt.preventDefault();
   const isGalleryImageEl = evt.target.classList.contains("gallery__image");
+  // проверяем, что кликнули именно на изображение в карточке
   if (!isGalleryImageEl) {
     return;
   }
   console.log(evt.target);
+  // создаем экземпляр lightbox с выбранным изображением
   const instance = basicLightbox.create(
     `<img src="${evt.target.dataset.source}" width="800" height="600">`,
     {
+      // добавляем обработчик события нажатия на клавишу Esc при открытом lightbox
       onShow: () => {
         window.addEventListener("keydown", onEscClick);
         console.log("onShow", instance);
       },
+      // удаляем обработчик события нажатия на клавишу Esc при открытом lightbox
       onClose: () => {
         window.removeEventListener("keydown", onEscClick);
         console.log("onClose", instance);
       },
     }
   );
-
+  
+  // открываем lightbox
   instance.show();
 
+  // функция обработки нажатия на клавишу Esc при открытом lightbox
   function onEscClick(evt) {
     if (evt.code === "Escape") {
       instance.close();
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,12 +1,19 @@
+// импортируем массив с данными для галереи из внешнего модуля
 import { galleryItems } from "./gallery-items.js";
 
+// выводим содержимое массива в консоль для проверки
+
 console.log(galleryItems);
+
+// получаем элемент контейнера галереи и генерируем разметку карточек с изображениями
 
 const imageContainer = document.querySelector(".gallery");
 const cardsMarkup = createImageCardsMarkup(galleryItems);
 
+// добавляем разметку карточек в контейнер галереи
 imageContainer.insertAdjacentHTML("beforeend", cardsMarkup);
 
+// функция для генерации разметки карточек изображений
 function createImageCardsMarkup(Items) {
   return Items.map(({ preview, original, description }) => {
     return `<li class="gallery__item">
@@ -17,6 +24,7 @@ function createImageCardsMarkup(Items) {
   }).join("");
 }
 
+// Далее код инициализирует объект SimpleLightbox и связывает его с изображениями, используя селектор '.gallery a', и присваивает ему параметры.
 new SimpleLightbox(".gallery a", {
   captionsData: "alt",
   captionDelay: 250,
